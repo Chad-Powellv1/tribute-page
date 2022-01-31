81c3496e47d8b89822517892df69ddd4b1829a0e
Chad Powell <chada.powell@outlook.com>
2022-01-31 16:22:53 +0000

    update index.html, style.css, main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,18 @@
 // Create variables connecting to the DOM in html
 let modalView = document.getElementById("bioModal");
 let modalBtn = document.getElementById("bioBtn");
-let modalExit = document.getElementsByClassName("bioClose");
+let modalExit = document.getElementsByClassName("bioClose")[0];
 
 /* Create a function opening the modal when users clicks the "Short Bio" button,
    followed by a corresponding event listener calling the function when clicked */
 let openModal = () => (modalView.style.display = "block");
 modalBtn.addEventListener("click", openModal);
 
-// Create a function closing the modal
-let closeModal = () => (modalView.style.display = "none");
+// Create a function to close the modal
+let closeModal = () => modalView.style.display = "none";
 
-// Create a function calling the closeModal function when the "X" is clicked
-modalExit.onclick = function() {
-    closeModal();
-};
-
-// Create a function closing the modal when the user click outside of the modal window
-window.onlcick = function() {
-    closeModal();
-};
+// Create event listener to close modal when user clicks "X" in modal
+modalExit.addEventListener("click", closeModal);
 
 // Create a function closing the modal when the user clicks the "Escape" key
 window.addEventListener("keydown", function(event) {
