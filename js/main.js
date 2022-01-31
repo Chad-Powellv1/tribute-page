a0570c692a8ccbdaaefa460702847128a35a32cb
Chad Powell <chada.powell@outlook.com>
2022-01-31 13:37:38 +0000

    updated index, style, and main files.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
-// JavaSript file for html modal
-
 // Create variables connecting to the DOM in html
-let modal = document.getElementById("bioModal");
+let modalView = document.getElementById("bioModal");
 let modalBtn = document.getElementById("bioBtn");
-let exit = document.getElementsByClassName("close");
+let modalExit = document.getElementsByClassName("bioClose");
 
-// Create a function opening the modal when users clicks the "Short Bio" button
-modalBtn.onclick = function() {
-    modal.style.display = "block";
-}
+/* Create a function opening the modal when users clicks the "Short Bio" button,
+   followed by a corresponding event listener calling the function when clicked */
+let openModal = () => (modalView.style.display = "block");
+modalBtn.addEventListener("click", openModal);
 
-// Create a function closing the modal when the user clicks the "X" in the modal window
-exit.onclick = function() {
-    modal.style.display = "none";
-}
+// Create a function closing the modal
+let closeModal = () => (modalView.style.display = "none");
+
+// Create a function calling the closeModal function when the "X" is clicked
+modalExit.onclick = function() {
+    closeModal();
+};
 
 // Create a function closing the modal when the user click outside of the modal window
-window.onclick = function(event) {
-    if (event.target !== modal) {
-        modal.style.display = "none";
-    }
-}
+window.onlcick = function() {
+    closeModal();
+};
 
 // Create a function closing the modal when the user clicks the "Escape" key
 window.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
-        modal.style.display = "none";
+        closeModal();
     }
-})
+});
